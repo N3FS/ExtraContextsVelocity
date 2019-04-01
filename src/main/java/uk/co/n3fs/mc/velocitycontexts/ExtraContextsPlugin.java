98bce5856e4b9ee8318a512f803799686cd65028
md678685 <mathew678@hotmail.co.uk>
2019-04-01 11:19:01 +0100

    Log version on startup

--- src/main/java/uk/co/n3fs/mc/velocitycontexts/ExtraContextsPlugin.java
+++ src/main/java/uk/co/n3fs/mc/velocitycontexts/ExtraContextsPlugin.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginDescription;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.ServerConnection;
@@ -35,6 +36,8 @@ public class ExtraContextsPlugin {
 
     @Subscribe
     public void onProxyInit(ProxyInitializeEvent event) {
+        logger.info("Starting ExtraContextsVelocity v" + getVersion());
+
         Optional<LuckPermsApi> lpOpt = LuckPerms.getApiSafe();
         if (!lpOpt.isPresent()) {
             throw new RuntimeException("LuckPerms is missing!");
@@ -50,5 +53,13 @@ public class ExtraContextsPlugin {
                 return accumulator;
             }
         });
+    }
+
+    private PluginDescription getDescription() {
+        return proxy.getPluginManager().getPlugin("extra-contexts").get().getDescription();
+    }
+
+    private String getVersion() {
+        return getDescription().getVersion().get();
     }
 }
